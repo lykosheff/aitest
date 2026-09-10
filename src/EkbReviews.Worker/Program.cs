ab17a685068e8be7ca0e5391bdd085e93b1dd0bf
Alex <lykoshev@gmail.com>
2026-09-11 03:42:14 +0500

    Register Ollama AI analyzer

--- src/EkbReviews.Worker/Program.cs
+++ src/EkbReviews.Worker/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddSingleton<IReviewAnalyzer, HeuristicReviewAnalyzer>();
+builder.Services.AddEkbReviewsInfrastructure(builder.Configuration);
 builder.Services.AddScoped<IReviewRepository, PostgresReviewRepository>();
 
 builder.Services.AddDbContext<EkbReviewsDbContext>(options =>
